2a8a6a39bc671f6c36147087f4c02daea660713e
Arman Misakordziyan <armanmisakordziyan@MacBook-Pro-Arman.local>
2025-06-20 13:28:38 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
+
+
 use App\Services\TelegramBotService;
+use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
-Route::post('/telegram/webhook', function () {
+Route::any('/telegram/webhook', function () {
     $update = Telegram::getWebhookUpdate();
+        Log::log(print_r($update, true));
     app(TelegramBotService::class)->handleWebhook($update);
     return response('OK', 200);
 });
